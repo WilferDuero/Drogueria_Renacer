184dc6849707798a64366724d5d46ed57dfac5dd
Wilfer Duero <dreadca@gmail.com>
2026-02-10 14:07:25 -0500

    Add reset reviews button (tests)

--- admin-sw.js
+++ admin-sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "admin-pwa-v16";
+const CACHE_NAME = "admin-pwa-v17";
 const ASSETS = [
   "/admin_login.html",
   "/admin.html",
   "/manifest-admin.json",
-  "/style.css?v=20260210d",
-  "/js/core.js?v=20260210d",
-  "/js/auth.js?v=20260210d",
-  "/js/admin.js?v=20260210d",
-  "/js/admin_pwa.js?v=20260210d",
+  "/style.css?v=20260210e",
+  "/js/core.js?v=20260210e",
+  "/js/auth.js?v=20260210e",
+  "/js/admin.js?v=20260210e",
+  "/js/admin_pwa.js?v=20260210e",
   "/assets/payments/Logo_Drogueria.svg",
   "/assets/payments/Logo_Coopidrogas.svg"
 ];
